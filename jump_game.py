4b9992252bfc9b193ea6ceb99ffaea835939ddb7
Kailash-Thiyagarajan <66647765+Kailash-Thiyagarajan@users.noreply.github.com>
2021-12-10 14:39:19 -0600

    Create jump_game.py

---
+++ jump_game.py
@@ -0,0 +1,31 @@
+
+"""
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+"""
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        
+        
+        if not nums:
+            return False
+        reachable = nums[0]
+        
+        for i in range(1, len(nums)):
+            if reachable < i:
+                return False
+            reachable = max(reachable , nums[i]+i)
+        return True
+        
+                
